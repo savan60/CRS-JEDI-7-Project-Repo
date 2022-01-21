6c38360b1492874754f0e874fa408591a58fa60e
savan60 <51092808+savan60@users.noreply.github.com>
2022-01-21 21:05:25 +0530

    bean file of card,cheque,netbanking,payment and service of payment added

---
+++ CRS-JEDI-7-Flipkart-POS/src/com/crs/flipkart/business/PaymentService.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package com.crs.flipkart.business;
+
+/**
+ * @author SAVAN
+ *
+ */
+public class PaymentService {
+	public void paymentNotify() {
+		System.out.println("Payment notification sent");
+	}
+}
