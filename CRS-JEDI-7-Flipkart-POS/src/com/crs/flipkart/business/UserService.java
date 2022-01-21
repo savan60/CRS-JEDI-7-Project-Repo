1bb04d8b3ab1cd46674d534e3e6714b7489be55a
pparth27743 <pparth27743@gmail.com>
2022-01-21 21:23:03 +0530

    UserService added

---
+++ CRS-JEDI-7-Flipkart-POS/src/com/crs/flipkart/business/UserService.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package com.crs.flipkart.business;
+
+/**
+ * @author parth
+ *
+ */
+public class UserService {
+	
+	public void forgotPassword() {
+		
+	}
+	
+	public void updatePassword() {
+			
+		}
+	
+	public void getUsers() {
+		
+	}
+	
+
+}
