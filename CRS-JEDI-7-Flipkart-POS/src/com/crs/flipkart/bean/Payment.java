523a6c8f0f3ce23f35372435926c6ed95065f3dd
savan60 <51092808+savan60@users.noreply.github.com>
2022-01-22 13:07:35 +0530

    payment.java is modified

--- CRS-JEDI-7-Flipkart-POS/src/com/crs/flipkart/bean/Payment.java
+++ CRS-JEDI-7-Flipkart-POS/src/com/crs/flipkart/bean/Payment.java
@@ -8,5 +8,57 @@ package com.crs.flipkart.bean;
  *
  */
 public class Payment {
-
+	private int studentId;
+	private String  invoiceId;
+	private int amount;
+	private boolean status; 
+	/**
+	 * @return the studentId
+	 */
+	public int getStudentId() {
+		return studentId;
+	}
+	/**
+	 * @param studentId the studentId to set
+	 */
+	public void setStudentId(int studentId) {
+		this.studentId = studentId;
+	}
+	/**
+	 * @return the invoiceId
+	 */
+	public String getInvoiceId() {
+		return invoiceId;
+	}
+	/**
+	 * @param invoiceId the invoiceId to set
+	 */
+	public void setInvoiceId(String invoiceId) {
+		this.invoiceId = invoiceId;
+	}
+	/**
+	 * @return the amount
+	 */
+	public int getAmount() {
+		return amount;
+	}
+	/**
+	 * @param amount the amount to set
+	 */
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+	/**
+	 * @return the status
+	 */
+	public boolean isStatus() {
+		return status;
+	}
+	/**
+	 * @param status the status to set
+	 */
+	public void setStatus(boolean status) {
+		this.status = status;
+	}
+	
 }
