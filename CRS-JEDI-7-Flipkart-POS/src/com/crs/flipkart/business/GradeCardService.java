7487139e263b4e110ab48484f5d620edd9989c0d
Nikhil <nikhil@arch>
2022-01-22 09:48:50 +0530

    created gradeCard and paymentNotifier

---
+++ CRS-JEDI-7-Flipkart-POS/src/com/crs/flipkart/business/GradeCardService.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.crs.flipkart.business;
+
+/**
+ * @author nikhil
+ *
+ */
+public class GradeCardService {
+
+	public void calGrade() {
+		System.out.println("Calculated Grade");
+	}
+}
