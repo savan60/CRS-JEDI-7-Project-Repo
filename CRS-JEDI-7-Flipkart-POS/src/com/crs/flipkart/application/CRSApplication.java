447de1c5227a1fa4f92839a366385dc6484c8a5e
aadarsh <aadarshcool5972@gmail.com>
2022-01-27 17:31:34 +0530

    CRS APPLICATION COMMIT

--- CRS-JEDI-7-Flipkart-POS/src/com/crs/flipkart/application/CRSApplication.java
+++ CRS-JEDI-7-Flipkart-POS/src/com/crs/flipkart/application/CRSApplication.java
@@ -65,6 +65,7 @@ public class CRSApplication {
 			Scanner sc=new Scanner(System.in);
 			int ch=sc.nextInt();
 			String email,password;
+			
 			UserInterface user=new UserService();
 			
 			switch(ch) {
