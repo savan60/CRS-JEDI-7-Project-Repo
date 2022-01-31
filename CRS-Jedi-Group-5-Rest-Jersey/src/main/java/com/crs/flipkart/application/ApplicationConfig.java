74e682876dee8a6c2ddf96b8404064ebb7c9119c
savan60 <51092808+savan60@users.noreply.github.com>
2022-01-31 16:10:06 +0530

    setup of rest project done

---
+++ CRS-Jedi-Group-5-Rest-Jersey/src/main/java/com/crs/flipkart/application/ApplicationConfig.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.crs.flipkart.application;
+
+import org.glassfish.jersey.server.ResourceConfig;
+
+//import org.glassfish.jersey.server.ResourceConfig;
+
+
+/**
+ * @author Mehak Goel
+ *
+ */
+
+
+
+public class ApplicationConfig extends ResourceConfig {
+
+	public ApplicationConfig() {
+
+//	register(CustomerController.class);
+
+
+	}
+
+}
